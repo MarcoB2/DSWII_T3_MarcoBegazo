4896faf6a3496762344ae651be042f00bd8847f4
MarcoB2 <begazomarcobegazo@gmail.com>
2023-11-25 21:26:46 -0500

    Primer commit :d

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_MarcoBegazo/DswiiT3MarcoBegazoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_MarcoBegazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3MarcoBegazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3MarcoBegazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_MarcoBegazo/DswiiT3MarcoBegazoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_MarcoBegazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3MarcoBegazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
